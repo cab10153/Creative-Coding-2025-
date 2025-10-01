2747f687f28060af920e19ce78fae4a28a8aa1af
cab10153 <cab10153@nyu.edu>
2025-10-01 01:11:31 -0400

    For assignment 2
    Lots of attempts at final assignment 2! Plus a test doc to see what worked and didn't

---
+++ assignment2test/sketch.js
@@ -0,0 +1,16 @@
+
+
+function setup() {
+  createCanvas(windowHeight, windowWidth);
+
+}
+
+function draw() {
+
+      background("rgba(234, 160, 41, 1)"); // for color picker
+  //  "rgb(0,0,0)"
+
+  fill("rgba(74, 197, 172, 1)");
+  rect(20, 5, 10, 100); // Draw square from origin
+
+}
