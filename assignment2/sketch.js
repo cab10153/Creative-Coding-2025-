a67946974191c59e96e3f9977c90c9bd29afda9e
cab10153 <cab10153@nyu.edu>
2025-10-01 11:26:46 -0400

    Final tweaks/ checked notes
    added some notes/ commentary/ final attempts at some changes

--- assignment2/sketch.js
+++ assignment2/sketch.js
@@ -1,47 +1,50 @@
-let circleColor
+
+let circleColor=("rgba(89, 206, 194, 1)"); // original color of circles
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	rectMode(CENTER); // rectangles can be drawn like circles / ellipses using this setting
-	 // for color picker
-  //  "rgb(0,0,0)"
-  circleColor=("rgba(89, 206, 194, 1)");
+	rectMode(CENTER); // rectangles can be drawn like circles (from the center)
+	
+	// for color picker  "rgb(0,0,0)"
+  //  note to self ^
 }
 
-function mousePressed() {
-  // changes to a random color
-  circleColor = color(random(255), random(255), random(255));
+function mousePressed() { //for some reasons only "mousePressed" works and not "mouseisPressed"
+
+  circleColor = color(random(255), random(255), random(255));  //changes circles to a random color every time mouse is presssed
 }
 
 function draw() {
 	background('#abee64ff'); //putting the background in draw clears the canvas at the start of the loop
-	strokeWeight(2); // outline thickness of shapes
-	stroke("rgba(161, 84, 223, 1)"); // outline color of shapes
+	strokeWeight(2); // outline thickness of both shapes
+	stroke("rgba(161, 84, 223, 1)"); // outline color of both shapes
 
+	//noLoop(); // prevents draw from looping
+	translate(50, 50); // move the start of the grid to coordinates 50,50 for pattern starting point
 
-	translate(50, 50); // move the coordinate grid to 50,50
-
-	for (let y = 0; y < height; y += 75) { // create a variable called y. loop the code below as long as y is less than height. increase y by 100 with each loop.
-		for (let x = 0; x < width; x += 100) {// create a variable called x. loop the code below as long as x is less than width. increase x by 100 with each loop.
+	for (let y = 0; y < height; y += 75) { // created a variable called y. loop the code below as long as y is less than height. increase y by 75 with each loop.
+		for (let x = 0; x < width; x += 100) {// created a variable called x. loop the code below as long as x is less than or equal to width. increases x by 100 with each loop.
 			push(); // container start
-			translate(x, y);
-			 fill(circleColor);
-            circle(0,0,50)
+			translate(x,y); // tells what to do with pattern using parameters set above
+			 fill(circleColor); // fills circle w/ original color
+            circle(0,0,50) // starts at translation origin point, 50 px diameter
 			pop(); // container end
 
 			push(); // container start
 			translate(x, y);
-      rotation = map(y,500,height -200,100,1.7);
-      rotate(rotation);
-	   fill("rgba(8, 243, 51, 1)");
-			 rect(0, 0, 5, 60); // Draw rec from origin
+      rotation = map(y,500,height -200,150,1.678); // rotate green rectangle based on these parameters
+      rotate(rotation); //uses above parameters to rotate green rect generatively on a loop
+	   fill("rgba(8, 243, 51, 1)"); // fills this rectangle green 
+			 rect(0, 0, 5, 60); // Draws rect from origin
 			pop();// container end
 
+
+let circleM = map(mouseY,250,height,0,255); //re-map height range to set where on grid the circle cursor changes opacity
+	fill(150,circleM); // fill purple with opacity set by circleM variables
 	
-			circle(mouseX, mouseY, width*0.078); // makes circle that moves with mouse cursor
-let circleM = map(mouseY,250,height,0,255); //remap height to a range of 0-255
-	fill(150,circleM); // fill purple with opacity set by circleM 
-	
-	
+	circle(mouseX, mouseY, width*0.078); // makes circle that moves with mouse cursor 
+	// however I cannot figure out how to center it 
+	// I tried several things, I think maybe it has to do with my code hierarchy?	
 
 		}
 		
