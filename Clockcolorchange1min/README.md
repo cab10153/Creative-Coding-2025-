5e27d0b59348d610fe0715b160e428334e630038
cab10153 <cab10153@nyu.edu>
2025-10-15 13:20:07 -0400

    Tried to add something
    Tried to make another one to show it in fast forward but didnt work, changed read me

---
+++ Clockcolorchange1min/README.md
@@ -0,0 +1,78 @@
+# template
+
+10/15/2025
+
+Made changes to what we started at our meeting. Branches grow every second, it resets every minutes, and at the hour mark it changes the color pallette of the mycelium. Also added some variables and made some aesthetic changes.
+
+10/8/2025
+
+As we spoke about in our meeting, I would like to recreate the concept
+of mycelium spreading from a starting point and branching off in different angles. The mycelium will grow according to seconds and minutes (maybe also milliseconds?) and then reset, resulting in a rapid growing network of mycelium across the screen. I am interested in also adding color, perhaps changing color according to seconds, minutes, etc. There are a few sources of inspiration that I'm using:
+
+https://commons.wikimedia.org/wiki/File:Mycelium_growth_simulation.gif
+
+https://www.youtube.com/watch?v=c6JRjVBYijs
+
+Here is also a link to the code that you made as an example for me during our meeting:
+
+https://openprocessing.org/sketch/2748199
+
+---
+
+This is a README file that can be used to describe and document your assignment.
+
+Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
+
+---
+
+# Heading1
+## Heading2
+### Heading3
+#### Heading4
+##### Heading5
+###### Heading6
+
+**bold text**
+
+*italicized text*
+
+~~strikethrough text~~
+
+Ordered List:
+1. First item
+2. Second item
+3. Third item
+
+Unordered List:
+- First item
+- Second item
+- Third item
+
+`short code block`
+
+```
+extended code block
+fun() {
+  return 0
+}
+```
+
+Link:  
+[linked text](https://www.example.com)
+
+
+Image with url:  
+![image description](https://dm-gy-6063-2024f-b.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
+
+
+Image on repo:  
+![image description](./file-name.jpg)
+
+
+To start a new line, add two spaces at the end of a line, like this:  
+this is a new line.
+
+
+To start a new paragraph, leave an empty line between two lines of text.
+
+This is a new paragraph.
---
+++ Clockcolorchange1min/sketch.js
@@ -0,0 +1,99 @@
+// Drunk Mycelium
+// used inspiration from these two animations
+//https://commons.wikimedia.org/wiki/File:Mycelium_growth_simulation.gif
+//https://www.youtube.com/watch?v=c6JRjVBYijs
+// also help from you based on our meeting :) Thanks again!
+
+let s; // establishes "s" variable globally
+let minute = 0; // Global variable holds minutes
+let myDrunks = []; // empty array to use later
+let myceColor; // variable I made with the purpose to change mycelium color 
+function setup() {
+	createCanvas(windowWidth, windowHeight); // Changed from 500 x 500 to windowWidth, windowHeight. I think it looks pretty nice in the browser
+  
+  myceColor = random(220,254); // gives it initial values of mycecolor
+
+	s = second(); 
+	for (let i = 0; i < s; i++) { // spawns first mycelium branch at set up
+		spawnNewDrunk(); // is this in setup because we are establishing the initial branch of Mycelium?
+	}
+	background(0);
+  
+}
+
+function draw() {
+
+	fill(255); //I took this out and realized it didn't work so I put it back in
+	
+  if (s != second()) {
+
+		console.log("new second " + s); // allows you to verify that the growth happening every second when you open the dev tools pane
+		if (second() == 0) {
+      minute = minute + 1; // adds new 
+			myDrunks = []; // why do we establish empty array again here?
+			background(0); // took this out and put it back in cause it didn't work
+		}
+
+		spawnNewDrunk();
+
+		for (let i = 0; i < myDrunks.length; i++) { // don't understand the length part because where have we established the length of myDrunks? I say this because globally it is an empty array
+			myDrunks[i].changeDirection(); // changes direction every second
+	  }
+
+		s = second(); // this is where it is established later on. Do we need it globally too?
+	}
+
+  if (minute == 1) { // proud of this one! Hopefully figured it out -- should change the mycelium color  every hour. I tested by trying it at "second=1" instead of minute=59 then changed it back
+    myceColor = (0,100,25);
+//    let minute = 0;
+   console.log // double checking what's going on
+  }
+
+
+	for (let i = 0; i < myDrunks.length; i++) { //again trying to figure out how we know what myDrunks length is. Is it the length of the canvas?
+		myDrunks[i].move(); // calling upon the move function from the Drunk class
+		myDrunks[i].display(); // calling upon the display function from the Drunk class
+	}
+}
+
+function spawnNewDrunk() {
+	let x = random(width); // I tried changing this value and it didnt seem to do much
+	let y = random(height);// Also tried changing this value and didn't seem to do much
+	let diameter = 2.5; // changed diameter from 3 to 2.5 because it looks cooler a bit thinner
+	let range = random(0,50);
+	let myDrunk = new Drunk(x, y, diameter, range);
+	myDrunks.push(myDrunk); // remember this from the class/ tutorial but still a bit confused about this concept
+}
+
+class Drunk {
+	constructor(x, y, diameter, range) { // the constructor establishes a blueprint for all of the functions located in the drunk class
+		this.x = width/2; // you must use "this." when establishing parameters in the class set up
+		this.y = height-100; // changed starting point from the bottom of the screen to 100 pixels above the bottom of the screen, I think that looks better
+		this.destinationX = this.x; 
+		this.destinationY = this.y;
+		this.diameter = diameter;
+		this.range = range;
+    this.myceColor = myceColor; //random(220,254); //made new variable myce.Color to replace "this.hue"
+	}
+
+	move() { // created a function in the Drunk class
+		this.x = lerp(this.x,this.destinationX,0.2); // truthfully not understanding lerp here. I read the tutorials I swear!!
+		this.y = lerp(this.y,this.destinationY,0.2); // I know it finds a middle point between something but i'm confused
+	}
+
+	display() { // created a function in the Drunk class
+		noStroke();
+		colorMode(HSB); // why HSB mode? 
+		
+		fill(this.myceColor,255,100,125); // I tried changing the brightness value but it didnt seem to do anything 
+		circle(this.x, this.y, this.diameter);
+	}
+	
+	changeDirection(){ // created a function in the drunk class
+			this.destinationX = this.destinationX+random(-this.range, this.range);
+		this.destinationY = this.destinationY-random(this.range); 
+		this.myceColor+=3; // when I changed the value here of  from +=1 to +=3 it comes out much more vibrant/ electric
+	}
+
+
+}
